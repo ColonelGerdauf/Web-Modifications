2b71558ab60cbe99837ed8e4979598e57b4f8a2e
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2015-05-16 21:24:04 -0400

    Update LTT_ignore_hider.user.js
    Metadata and code optimization.

--- LTT_ignore_hider.user.js
+++ LTT_ignore_hider.user.js
@@ -1,16 +1,24 @@
 // ==UserScript==
 // @name         LTT Ignored Post Hider
-// @version      1.01
+// @namespace    https://github.com/ColonelGerdauf
+// @version      1.02
 // @author       Colonel_Gerdauf
-// @description  Hides the posts made by members in your ignore list instead of displaying a short prompt, within the open threads.
-// @include      /^https?:\/\/(?:[^\.]+\.)?linustechtips\.com\/main\/topic\/.*$/
+// @description  Provides the option of hiding the posts made by members in your ignore list, replacing the short prompt, within the open threads. It also displays the original post of a thread, even if the author is ignored.
+// @match        *://linustechtips.com/main/topic/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @updateURL    https://github.com/ColonelGerdauf/Web-Modifications/raw/master/LTT_ignore_hider.user.js
 // @downloadURL  https://github.com/ColonelGerdauf/Web-Modifications/raw/master/LTT_ignore_hider.user.js
 // ==/UserScript==
 
 var $jq = jQuery.noConflict();
 var bin = GM_getValue("lgnore_switch", true);
+
+$jq(".topic.hfeed.clear.clearfix > .maintitle")
+.append($jq("<span>", {id:"lttn-ignore", style:'font-size: 13px; float: right;'})
+        .append($jq("<a>", {href:'#', style:'padding-left: 5px;', id:'lttn-ignore-data'})
+                .text(binaryFeedback(bin))));
 
 if ($jq('.post_block').first().find('a[itemprop="replyToUrl"]').text().trim() === "#1")
 {
@@ -24,34 +32,21 @@ if ($jq('.post_block').first().find('a[itemprop="replyToUrl"]').text().trim() ==
     }
 }
 
-if (bin === true)
+if (bin)
 {
-    $jq(".post_block").each(function(){
-        if ($jq(this).children(".post_ignore").length)
-        {
-            $jq(this).parent().replaceWith("<!--ignore.enabled-->");
-        }
+    $jq(".post_ignore").each(function(){
+        $jq(this).parent().parent().replaceWith("<!--ignore.enabled-->");
     });
 }
 
-$jq(".topic.hfeed.clear.clearfix > .maintitle")
-.append($jq("<span>", {id:"lttn-ignore", style:'font-size: 13px; float: right;'})
-.append($jq("<a>", {href:'#', style:'padding-left: 5px;', id:'lttn-ignore-data'}).text(binaryFeedback(bin))));
-
 $jq("#lttn-ignore-data").click(function(event) {
     event.preventDefault();
-    bin = !bin;
-    GM_setValue("lgnore_switch", bin);
+    GM_setValue("lgnore_switch", !bin);
+    $jq(this).text(binaryFeedback(!bin));
     location.reload();
-    $jq(this).text(binaryFeedback(bin));
 });
 
 function binaryFeedback($bin)
 {
-    if (bin === false) {
-        return "Hide Ingored";
-    }
-    else {
-        return "Show Ignored";
-    }
+    return bin ? "Show Ingored" : "Hide Ignored";
 }
