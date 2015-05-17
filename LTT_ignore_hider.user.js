9c52901a3ecee2d4f300f09b1bd4e4d519490b7f
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2015-05-17 14:38:17 -0400

    Update LTT_ignore_hider.user.js
    Further code optimization.

--- LTT_ignore_hider.user.js
+++ LTT_ignore_hider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LTT Ignored Post Hider
 // @namespace    https://github.com/ColonelGerdauf
-// @version      1.03
+// @version      1.04
 // @author       Colonel_Gerdauf
 // @description  Provides the option of hiding the posts made by members in your ignore list, replacing the short prompt, within the open threads. It also displays the original post of a thread, even if the author is ignored.
 // @match        *://linustechtips.com/main/topic/*
@@ -14,12 +14,12 @@
 
 var $jq = jQuery.noConflict();
 
-if ($jq(".main_width").children(".logged_in").length)
+if ($jq('.main_width').children('.logged_in').length)
 {
-    var bin = GM_getValue("lgnore_switch", true);
+    var bin = GM_getValue('lgnore_switch', true);
 
     $jq(".topic.hfeed.clear.clearfix > .maintitle")
-    .append($jq("<span>", {id:"lttn-ignore", style:'font-size: 13px; float: right;'})
+    .append($jq("<span>", {id:'lttn-ignore', style:'font-size: 13px; float: right;'})
             .append($jq("<a>", {href:'#', style:'padding-left: 5px;', id:'lttn-ignore-data'})
                     .text(binaryFeedback(bin))));
 
@@ -28,24 +28,23 @@ if ($jq(".main_width").children(".logged_in").length)
         if ($jq('.post_block').first().children(".post_ignore").length)
         {
             var pid = $jq('.post_block').first().attr('id').replace("post_id_","");
-            $jq(".post_ignore").first().replaceWith("<!--ignore.firstpost:"+pid+"-->");
-            $jq(".post_wrap").first().css("display", "");
-            $jq(".post_wrap").first().css("overflow", "visible");
+            $jq('.post_ignore').first().replaceWith(["<!--ignore.firstpost:", pid, "-->"].join(''));
+            $jq('.post_wrap').first().css('display', '');
+            $jq('.post_wrap').first().css('overflow', 'visible');
             $jq('a[itemprop="replyToUrl"]').first().text("Ignored Original Poster - #1");
         }
     }
 
     if (bin)
     {
-        $jq(".post_ignore").each(function(){
+        $jq('.post_ignore').each(function(){
             $jq(this).parent().parent().replaceWith("<!--ignore.enabled-->");
         });
     }
 
-    $jq("#lttn-ignore-data").click(function(event) {
+    $jq('#lttn-ignore-data').click(function(event) {
         event.preventDefault();
-        GM_setValue("lgnore_switch", !bin);
-        $jq(this).text(binaryFeedback(!bin));
+        GM_setValue('lgnore_switch', !bin);
         location.reload();
     });
 
