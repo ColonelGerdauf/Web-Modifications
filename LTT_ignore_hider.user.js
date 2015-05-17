c54ba920842d8431c983b67985e81549886ac39a
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2015-05-16 22:20:42 -0400

    Update LTT_ignore_hider.user.js
    Added login limitation.

--- LTT_ignore_hider.user.js
+++ LTT_ignore_hider.user.js
@@ -13,40 +13,44 @@
 // ==/UserScript==
 
 var $jq = jQuery.noConflict();
-var bin = GM_getValue("lgnore_switch", true);
 
-$jq(".topic.hfeed.clear.clearfix > .maintitle")
-.append($jq("<span>", {id:"lttn-ignore", style:'font-size: 13px; float: right;'})
-        .append($jq("<a>", {href:'#', style:'padding-left: 5px;', id:'lttn-ignore-data'})
-                .text(binaryFeedback(bin))));
-
-if ($jq('.post_block').first().find('a[itemprop="replyToUrl"]').text().trim() === "#1")
+if ($jq(".main_width").children(".logged_in").length)
 {
-    if ($jq('.post_block').first().children(".post_ignore").length)
+    var bin = GM_getValue("lgnore_switch", true);
+
+    $jq(".topic.hfeed.clear.clearfix > .maintitle")
+    .append($jq("<span>", {id:"lttn-ignore", style:'font-size: 13px; float: right;'})
+            .append($jq("<a>", {href:'#', style:'padding-left: 5px;', id:'lttn-ignore-data'})
+                    .text(binaryFeedback(bin))));
+
+    if ($jq('.post_block').first().find('a[itemprop="replyToUrl"]').text().trim() === "#1")
     {
-        var pid = $jq('.post_block').first().attr('id').replace("post_id_","");
-        $jq(".post_ignore").first().replaceWith("<!--ignore.firstpost:"+pid+"-->");
-        $jq(".post_wrap").first().css("display", "");
-        $jq(".post_wrap").first().css("overflow", "visible");
-        $jq('a[itemprop="replyToUrl"]').first().text("Ignored Original Poster - #1");
+        if ($jq('.post_block').first().children(".post_ignore").length)
+        {
+            var pid = $jq('.post_block').first().attr('id').replace("post_id_","");
+            $jq(".post_ignore").first().replaceWith("<!--ignore.firstpost:"+pid+"-->");
+            $jq(".post_wrap").first().css("display", "");
+            $jq(".post_wrap").first().css("overflow", "visible");
+            $jq('a[itemprop="replyToUrl"]').first().text("Ignored Original Poster - #1");
+        }
     }
-}
 
-if (bin)
-{
-    $jq(".post_ignore").each(function(){
-        $jq(this).parent().parent().replaceWith("<!--ignore.enabled-->");
+    if (bin)
+    {
+        $jq(".post_ignore").each(function(){
+            $jq(this).parent().parent().replaceWith("<!--ignore.enabled-->");
+        });
+    }
+
+    $jq("#lttn-ignore-data").click(function(event) {
+        event.preventDefault();
+        GM_setValue("lgnore_switch", !bin);
+        $jq(this).text(binaryFeedback(!bin));
+        location.reload();
     });
-}
 
-$jq("#lttn-ignore-data").click(function(event) {
-    event.preventDefault();
-    GM_setValue("lgnore_switch", !bin);
-    $jq(this).text(binaryFeedback(!bin));
-    location.reload();
-});
-
-function binaryFeedback($bin)
-{
-    return bin ? "Show Ingored" : "Hide Ignored";
+    function binaryFeedback($bin)
+    {
+        return bin ? "Show Ingored" : "Hide Ignored";
+    }
 }
