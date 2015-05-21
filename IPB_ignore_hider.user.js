7b848378f899d70bc816b1b897bd558ec68c51d7
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2015-05-20 22:04:15 -0400

    Update and rename LTT_ignore_hider.user.js to IPB_ignore_hider.user.js
    Extended compatibility to all websites following the IPS forum URL patterns
    Included a a text notice to indicate whether the user is logged in or not

--- IPB_ignore_hider.user.js
+++ IPB_ignore_hider.user.js
@@ -1,27 +1,28 @@
 // ==UserScript==
-// @name         LTT Ignored Post Hider
+// @name         IPB Ignored Post Hider
 // @namespace    https://github.com/ColonelGerdauf
-// @version      1.051
+// @version      1.10
 // @author       Colonel_Gerdauf
 // @description  Provides the option of hiding the posts made by members in your ignore list, replacing the short prompt, within the open threads. It also displays the original post of a thread, even if the author is ignored.
-// @match        *://linustechtips.com/main/topic/*
+// @match        http*://*/topic/*
+// @match        http*://*/*/topic/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @grant        GM_getValue
 // @grant        GM_setValue
-// @updateURL    https://github.com/ColonelGerdauf/Web-Modifications/raw/master/LTT_ignore_hider.user.js
-// @downloadURL  https://github.com/ColonelGerdauf/Web-Modifications/raw/master/LTT_ignore_hider.user.js
+// @updateURL    https://github.com/ColonelGerdauf/Web-Modifications/raw/master/IPB_ignore_hider.user.js
+// @downloadURL  https://github.com/ColonelGerdauf/Web-Modifications/raw/master/IPB_ignore_hider.user.js
 // @supportURL   https://github.com/ColonelGerdauf/Web-Modifications/issues
 // ==/UserScript==
 
 var $jq = jQuery.noConflict();
 
-if ($jq('.main_width').children('.logged_in').length)
+if ($jq('#user_navigation').hasClass('logged_in') || $jq('#branding-user > .ipsList_inline.right').children('#nav-user').length)
 {
     var bin = GM_getValue('lgnore_switch', false);
 
-    $jq('.topic.hfeed.clear.clearfix > .maintitle')
-    .append($jq('<span>', {id:'lttn-ignore', style:'font-size: 13px; float: right;'})
-            .append($jq('<a>', {href:'javascript:;', class:'lttn-ignore-data', style:'padding-left: 5px;'})
+    $jq('.maintitle.clear.clearfix')
+    .append($jq('<span>', {id:'ips-ignore', class:'ipsType_small', style:'float: right;'})
+            .append($jq('<a>', {href:'javascript:;', class:'ips-ignore-data', style:'padding-left: 5px;'})
                     .text(binaryFeedback(bin))));
 
     if ($jq('.post_block').first().find('a[itemprop="replyToUrl"]').text().trim() === "#1")
@@ -43,10 +44,16 @@ if ($jq('.main_width').children('.logged_in').length)
         });
     }
 
-    $jq('.lttn-ignore-data').click(function() {
+    $jq('.ips-ignore-data').click(function(){
         GM_setValue('lgnore_switch', !bin);
         location.reload();
     });
+}
+else if ($jq('#user_navigation').hasClass('not_logged_in') || $jq('#branding-user').children('#nav-guest').length)
+{
+    $jq('.maintitle.clear.clearfix')
+    .append($jq('<span>', {class:'ipsType_small', style:'float: right; padding-left: 5px;'})
+            .text("FUNCTION DISABLED : NOT LOGGED IN"));
 }
 
 function binaryFeedback($bin)
