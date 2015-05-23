447a317431f475e5a8e40cb885252f7d56f4dda8
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2015-05-22 22:29:43 -0400

    Update IPB_ignore_hider.user.js
    Inclusion of animation 
    Return of ability to display original ignored post
    Previously omitted versioning

--- IPB_ignore_hider.user.js
+++ IPB_ignore_hider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IPB Ignored Post Hider
 // @namespace    https://github.com/ColonelGerdauf
-// @version      1.10
+// @version      1.12
 // @author       Colonel_Gerdauf
 // @description  Provides the option of hiding the posts made by members in your ignore list, replacing the short prompt, within the open threads. It also displays the original post of a thread, even if the author is ignored.
 // @match        *://*/topic/*
@@ -22,26 +22,34 @@ if ($jq('#user_navigation').hasClass('logged_in') || $jq('#branding-user > .ipsL
 
     $jq('.maintitle.clear.clearfix')
     .append($jq('<span>', {id:'ips-ignore', class:'ipsType_small', style:'float: right;'})
-            .append($jq('<a>', {href:'javascript:;', class:'ips-ignore-data', style:'padding-left: 5px;'})
+            .append($jq('<a>', {href:'javascript:void(0);', class:'ips-ignore-data', style:'padding-left: 5px;'})
                     .text(binaryFeedback(bin))));
 
     var ignoredPosts = $jq(".post_ignore").closest(".paper-card");
+    
     if ($jq('.post_block').first().find('a[itemprop="replyToUrl"]').text().trim() === "#1")
     { //If the first post is on this page, we want to make sure we don't hide it
-        ignoredPosts = $jq(".post_block:not(:eq(0)) .post_ignore").closest(".paper-card");
+        ignoredPosts = $jq('.post_block:not(:eq(0)) .post_ignore').closest('.paper-card');
+        
+        var firstPost = $jq('.post_block').first();        
+        
+        firstPost.find('.post_ignore').replaceWith("<!--ignore.firstpost-->");
+        firstPost.find('.post_wrap').show();
+        firstPost.find('.post_wrap').css('overflow', 'visible');
+        firstPost.find('a[itemprop="replyToUrl"]').text("Ignored Original Poster - #1");
     }
 
     if (bin)
     {
-        ignoredPosts.hide();
+        ignoredPosts.hide('fast');
     }
 
     $jq('.ips-ignore-data').click(function(){
         bin = !bin;
         GM_setValue('lgnore_switch', bin);
-        if(bin) ignoredPosts.hide();
-        else ignoredPosts.show();
-        $jq(".ips-ignore-data").text(binaryFeedback(bin));
+        if(bin) ignoredPosts.hide('fast');
+        else ignoredPosts.show('fast');
+        $jq(this).text(binaryFeedback(bin));
     });
 }
 else if ($jq('#user_navigation').hasClass('not_logged_in') || $jq('#branding-user').children('#nav-guest').length)
@@ -53,5 +61,5 @@ else if ($jq('#user_navigation').hasClass('not_logged_in') || $jq('#branding-use
 
 function binaryFeedback($bin)
 {
-    return bin ? "Show Ingored" : "Hide Ignored";
+    return bin ? "Show Ignored" : "Hide Ignored";
 }
