7e9d7e2da89cae0bd4c10cd456fe930042e60b83
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2015-08-04 21:52:20 -0400

    Algorithm Optimization
    The ignored post highlight has been altered as to display the number being referenced per post, instead of every number on the page. Also, redundant code has been removed.

--- IPB_ignore_hider.user.js
+++ IPB_ignore_hider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IPB Ignored Post Hider
 // @namespace    https://github.com/ColonelGerdauf
-// @version      1.2.1
+// @version      1.2.2
 // @author       Colonel_Gerdauf
 // @contributor  StormDrive
 // @description  Provides the option of hiding the posts made by members in your ignore list, replacing the short prompt, within the open threads. It also displays the original post of a thread, even if the author is ignored.
@@ -16,19 +16,18 @@
 // @supportURL   https://github.com/ColonelGerdauf/Web-Modifications/issues
 // ==/UserScript==
 
-
 var $jq = jQuery.noConflict();
 
 var domainStr = ["lgnore_switch_domain", document.domain].join(":");
 
 if ($jq('#user_navigation').hasClass('logged_in') || $jq('#branding-user > .ipsList_inline.right').children('#nav-user').length)
 {
-    var bin = GM_getValue(domainStr, false);
+    var bin = GM_getValue(domainStr, 0);
 
     $jq('.maintitle.clear.clearfix')
     .append($jq('<span>', {id:'ips-ignore', class:'ipsType_small', style:'float: right;'})
-            .append($jq('<a>', {href:'javascript:void(0);', class:'ips-ignore-data', style:'padding-left: 5px;'})
-                    .text(binaryFeedback(bin))));
+            .append($jq('<a>', {href:'javascript:void(0)', class:'ips-ignore-data', style:'padding-left: 5px;'})
+                    .text(ternaryFeedback(bin))));
 
     var firstPost = $jq('.post_block').first(); 
 
@@ -44,29 +43,54 @@ if ($jq('#user_navigation').hasClass('logged_in') || $jq('#branding-user > .ipsL
 
     var ignoredPosts = $jq(".post_ignore").closest(".paper-card");
 
-    if (bin)
+    if (bin === 1)
     {
         ignoredPosts.hide('fast');
     }
+    else if (bin === 2)
+    {
+
+        ignoredPosts.find('.post_ignore').hide('fast');
+        ignoredPosts.find('.post_wrap').show('fast');
+        ignoredPosts.find('a[itemprop="replyToUrl"]').prepend("Ignored Poster - ");
+    }
 
     $jq('.ips-ignore-data').click(function(){
-        bin = !bin;
+
+        bin++;
+        bin %= 3;
         GM_setValue(domainStr, bin);
-        if(bin) ignoredPosts.hide('fast');
-        else ignoredPosts.show('fast');
-        $jq(this).text(binaryFeedback(bin));
+
+        switch (bin) 
+        {
+            case 0:
+                ignoredPosts.show('fast');
+                ignoredPosts.find('.post_ignore').show('fast');
+                ignoredPosts.find('.post_wrap').hide('fast');
+                ignoredPosts.find('a[itemprop="replyToUrl"]').replace("Ignored Poster - ","");
+                break;
+            case 1:
+                ignoredPosts.hide('fast');
+                break;
+            case 2:
+                ignoredPosts.show('fast');
+                ignoredPosts.find('.post_ignore').hide('fast');
+                ignoredPosts.find('.post_wrap').show('fast');
+                ignoredPosts.find('a[itemprop="replyToUrl"]').prepend("Ignored Poster - ");
+                break;
+        }
+
+        $jq(this).text(ternaryFeedback(bin));
     });
 }
 else if ($jq('#elUserNav').hasClass('cSignedIn'))
 {    
-    var bin = GM_getValue(domainStr, false);
+    var bin = GM_getValue(domainStr, 0);
 
     $jq('.ipsType_sectionTitle')
     .append($jq('<span>', {id:'ips-ignore', style:'float: right;'})
             .append($jq('<a>', {href:'javascript:void(0)', class:'ips-ignore-data', style:'padding-left: 5px;'})
-                    .text(binaryFeedback(bin))));
-
-
+                    .text(ternaryFeedback(bin))));
 
     if ($jq('li.ipsPagination_page.ipsPagination_active').find('a').attr('data-page') === "1")
     { //If the first post is on this page, we want to make sure we don't hide it    
@@ -83,18 +107,47 @@ else if ($jq('#elUserNav').hasClass('cSignedIn'))
     }
 
     var ignoredPosts = $jq(".ipsComment_ignored");
+    var ignoredPostID  = ignoredPosts.attr('data-ignorecommentid');
 
-    if (bin)
+    alert (ignoredPostID);
+
+    if (bin === 1)
     {
         ignoredPosts.hide('fast');
     }
+    else if (bin === 2)
+    {
+        ignoredPosts.hide('fast');
+        $jq('.cPost.ipsComment').hasClass('ipsHide');
+        ignoredPosts.parent().find(ignoredPostID).find('.ipsType_reset:not(.ipsType_blendLinks)')
+        .append($jq('<span>', {class:'ips-ignore-tab', style:' float: right; padding-left: 5px;'})
+                .text("Ignored Poster"));
+    }
 
     $jq('.ips-ignore-data').click(function(){
-        bin = !bin;
+
+        bin++;
+        bin %= 3;
         GM_setValue(domainStr, bin);
-        if(bin) ignoredPosts.hide('fast');
-        else ignoredPosts.show('fast');
-        $jq(this).text(binaryFeedback(bin));
+
+        switch (bin) 
+        {
+            case 0:
+                ignoredPosts.show('fast');
+                ignoredPosts.parent().find(ignoredPostID).addClass('ipsHide');
+                ignoredPosts.parent().find(ignoredPostID).find('.ips-ignore-tab').remove();
+                break;
+            case 1:
+                ignoredPosts.hide('fast');
+                break;
+            case 2:
+                ignoredPosts.hide('fast');
+                $jq('.cPost.ipsComment').removeClass('ipsHide');
+                ignoredPosts.parent().find(ignoredPostID).find('.ipsType_reset:not(.ipsType_blendLinks)')
+                .append($jq('<span>', {class:'ips-ignore-tab', style:' float: right; padding-left: 5px;'})
+                        .text("Ignored Poster"));
+        }
+        $jq(this).text(ternaryFeedback(bin));
     });
 }
 else if ($jq('#user_navigation').hasClass('not_logged_in') || $jq('#branding-user').children('#nav-guest').length)
@@ -110,7 +163,21 @@ else if ($jq('#elUserNav').hasClass('cSignedOut'))
             .text("FUNCTION DISABLED : NOT LOGGED IN")); 
 }
 
-function binaryFeedback($bin)
+function ternaryFeedback(bin)
 {
-    return bin ? "Show Ignored" : "Hide Ignored";
+    var data = "Ignored";
+    var dataType = "NULL";
+
+    switch (bin) {
+        case 0:
+            dataType = "Off";
+            break;
+        case 1:
+            dataType = "Hide";
+            break;
+        case 2:
+            dataType = "Show";
+            break;
+    }
+    return [data,dataType].join(": ");
 }
